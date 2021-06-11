d9c27f29e2f57f2eceaf5284a6211a0fa9eb71df
ayredkklc <ayredkklc@gmail.com>
2021-06-11 18:37:49 -0400

    hi

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, send_from_directory
 from dotenv import load_dotenv
+import csv
+from flask import Flask, render_template, url_for, request, redirect
 
 load_dotenv()
 app = Flask(__name__)
@@ -8,4 +10,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="Derya Kilic", url=os.getenv("URL"))
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as csvfile:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        writer = csv.writer(csvfile)
+        writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['GET', 'POST'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'Something went wrong!'
