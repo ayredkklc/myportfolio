203df0845eecabe8e65b524db3b7a02ded554fa1
ayredkklc <ayredkklc@gmail.com>
2021-07-13 19:19:35 -0400

    fixed the issues with flake8 and black

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,41 @@
 import os
-from flask import Flask, render_template, send_from_directory
+# from flask import Flask, render_template, send_from_directory
 from dotenv import load_dotenv
 import csv
-from flask import Flask, render_template, url_for, request, redirect
-#from . import db
+from flask import Flask, render_template, request, redirect
+
+# from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-#from app.db import get_db
+
+# from app.db import get_db
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 
-
-## Rest of the file
+# Rest of the file
 load_dotenv()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-    user=os.getenv('POSTGRES_USER'),
-    passwd=os.getenv('POSTGRES_PASSWORD'),
-    host=os.getenv('POSTGRES_HOST'),
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
     port=5432,
-    table=os.getenv('POSTGRES_DB'))
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-#app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-#db.init_app(app)
+# app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+# db.init_app(app)
+
 
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -41,56 +46,63 @@ class UserModel(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
-@app.route('/')
+
+
+@app.route("/")
 def index():
-    return render_template('index.html', title="Derya Kilic", url=os.getenv("URL"))
+    return render_template("index.html", title="Derya Kilic", url=os.getenv("URL"))
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/projects')
+
+@app.route("/projects")
 def projects():
-    return render_template('projects.html')
+    return render_template("projects.html")
 
-@app.route('/contact')
+
+@app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    return render_template("contact.html")
 
-@app.route('/health')
+
+@app.route("/health")
 def health():
-    return 'Correct not hfjhs', 200
+    return "Correct not hfjhs", 200
+
 
 def write_to_csv(data):
-    with open('database.csv', 'a', newline='') as csvfile:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
+    with open("database.csv", "a", newline="") as csvfile:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
         writer = csv.writer(csvfile)
         writer.writerow([email, subject, message])
 
-@app.route('/submit_form', methods=['GET', 'POST'])
+
+@app.route("/submit_form", methods=["GET", "POST"])
 def submit_form():
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.form.to_dict()
         write_to_csv(data)
-        return redirect('/thankyou.html')
+        return redirect("/thankyou.html")
     else:
-        return 'Something went wrong!'
+        return "Something went wrong!"
 
 
-
-@app.route('/register', methods=('GET', 'POST'))
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -106,18 +118,18 @@ def register():
     return render_template("register.html")
 
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
--- app/db.py
+++ app/db.py
@@ -6,43 +6,44 @@ from flask.cli import with_appcontext
 
 
 def get_db():
-    if 'db' not in g:
+    if "db" not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
 
     return g.db
 
+
 def init_db():
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf8"))
+
 
 def close_db(e=None):
-    db = g.pop('db', None)
+    db = g.pop("db", None)
 
     if db is not None:
         db.close()
 
-## Rest of the file
-## Rest of the file
+
+# Rest of the file
+# Rest of the file
+
 
 def init_app(app):
-    #app.teardown_appcontext() tells Flask to call that function when cleaning up after returning the response.
+    # app.teardown_appcontext() tells Flask to call that function when cleaning up after returning the response.
     app.teardown_appcontext(close_db)
 
-    #app.cli.add_command() adds a new command that can be called with the flask command.
+    # app.cli.add_command() adds a new command that can be called with the flask command.
     app.cli.add_command(init_db_command)
 
 
-
-
-@click.command('init-db')
+@click.command("init-db")
 @with_appcontext
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
-    click.echo('Initialized the database.')
+    click.echo("Initialized the database.")
